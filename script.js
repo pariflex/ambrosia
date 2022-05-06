db33bccbb9e1f2d77cb8c18068c6f675721d0b25
pariflex <49361191+pariflex@users.noreply.github.com>
2022-05-07 00:22:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const html = document.documentElement;
 const canvas = document.getElementById("hero-lightpass");
 const context = canvas.getContext("2d");
 
-const frameCount = 53;
+const frameCount = 73;
 const currentFrame = index => (
   `http://trendyclues.xyz/wp-content/uploads/2022/04/0092.webp/${index.toString().padStart(4, '0')}.webp`
 )
